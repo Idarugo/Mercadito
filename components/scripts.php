50ec63a2ec929fa837892ba4a70675bbd5f09132
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2022-10-15 14:31:22 -0300

    css form creado

--- components/scripts.php
+++ components/scripts.php
@@ -2,7 +2,6 @@
     const redirectTo = (path) => {
         if(location.hostname.includes('localhost')){
             if (safari != undefined) { 
-                console.log('HOLA');
                 location.assign(`http://localhost:8080/Mercadito/${path}`);
             }
                 location.assign(`http://localhost/Mercadito/${path}`);
