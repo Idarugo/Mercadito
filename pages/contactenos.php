9db323e61198d541063a0d43b910b0976523b3ce
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2022-10-15 15:28:52 -0300

    Caja Formulario

--- pages/contactenos.php
+++ pages/contactenos.php
@@ -4,20 +4,30 @@
     <?php include '../components/head.php' ?>
     <link rel="stylesheet" href="../assets/styles/main.css">
     <link rel="stylesheet" href="../assets/styles/pages/contactenos.css">
+    <link rel="stylesheet" href="../assets/styles/components/forms.css">
 </head>
 <body>
 <?php include '../components/header.php' ?>
     <div class="container">
-        <h1>Contactanos</h1>
+        <p>Contactanos</p>
     </div>
     <section class="container-fluid banner">
         <div class="banner__img"></div>
         <h1>¡CONTÁCTANOS!</h1>
-        <p>Le damos vida a tus espacios.</p>
+        <p>Escríbenos y en breve los pondremos en contacto contigo</p>
     </section>
-    <div class="container container-main">
-        
+    <div class="caja-formulario">
+        <form>
+            <label for="nombre">Nombre</label>
+            <input id="nombre" name="nombre" placeholder="Nombre completo">
+            <label for="email">Correo Electronico</label>
+            <input id="email" name="email" type="email" placeholder="ejemplo@email.com">
+            <label for="mensaje">Mensaje</label>
+            <textarea id="mensaje" name="mensaje" placeholder="Danos tu mensaje"></textarea>
+            <input id="submit" name="submit" type="submit" value="Enviar">
+        </form>
     </div>
+
     <?php include '../components/footer.php' ?>
 </body>
 </html>
