6713a798b0343c6eba911ff9467703facc5f8f92
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2022-10-15 14:19:27 -0300

    bug puerto para marcelo intento 1

--- components/scripts.php
+++ components/scripts.php
@@ -1,7 +1,12 @@
 <script>
     const redirectTo = (path) => {
         if(location.hostname.includes('localhost')){
-            location.assign(`http://www.localhost/Mercadito/${path}`);
+            if (navigator.userAgent.toLowerCase().indexOf('safari') != -1) { 
+                if (navigator.userAgent.toLowerCase().indexOf('chrome') <= -1) {
+                    location.assign(`http://localhost:8080/Mercadito/${path}`);
+                } 
+                location.assign(`http://localhost/Mercadito/${path}`);
+            }
         }else{
             location.assign(`${location.hostname}/${path}`);
         } 
