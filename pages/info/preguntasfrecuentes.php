1ac90f0b8848ca34cc2d2f3d35caf114ba90d85d
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2022-10-15 21:47:56 -0300

    Preguntas frecuentes by Ikeda harket

--- pages/info/preguntasfrecuentes.php
+++ pages/info/preguntasfrecuentes.php
@@ -3,59 +3,62 @@
 <head>
     <?php include '../../components/head.php' ?>
     <link rel="stylesheet" href="../../assets/styles/main.css">
-    <link rel="stylesheet" href="/assets/styles/pages/info/preguntasfrecuentes.css">
+    <link rel="stylesheet" href="../../assets/styles/pages/info/preguntasfrecuentes.css">
 </head>
 <body>
     <?php include '../../components/header.php' ?>
-    <div class="container container-main">
-        <h1>Preguntas Frecuentes</h1>
+    <div class="container mb-4 container-main">
+        <h1 class="text-center mt-5">Preguntas Frecuentes</h1>
 
-        <h4>CUIDADO DE PLANTAS</h4>
+        <div class="row justify-content-center mt-5">
+            <div class="col-12 col-xl-10">
+                <h2 class="mb-3">CUIDADO DE PLANTAS</h2>
 
-        <h5>He tenido plantas anteriormente y se me han muerto, ¿Cómo me ayudará PlantMe para cuidarlas mejor?</h5>
-        <p>Todas nuestras plantas tienen su descripción individual, las cuales describen los espacios donde podrás establecer tu planta; como también, los principales cuidados que deben tener en términos de temperaturas, luz y riego. </p>
-        
-        <h5>¿Son nuestras plantas difíciles de mantener?</h5>
-        <p>Todas las plantas requieren de cuidados, si bien hay algunas que requieren cuidados más complejos, existen otras que son de muy fácil cuidado, ¨Las resistentes¨. A pesar de que estas requieren de pocos cuidados, todavía no hemos encontrado un tipo de planta que no requiera de algún cuidado.</p>
-        
-        <p>De todas maneras, en nuestra página encontrarás información importante para hacer del cuidado de tus plantas lo más fácil posible. Además, con nuestros videos podrás aprender suficientes tips para convertirte en un experto en el cuidado de plantas.</p>
-        
-        <h5>¿Mi planta seguirá viva a la vuelta de mis vacaciones?</h5>
-        <p>Esto no se podrá saber con seguridad, y la respuesta a esta pregunta dependerá de distintos factores: de la cantidad de tiempo que no recibirá cuidado tu planta, del clima mientras está lejos, de la posición de las plantas con respecto al sol. Sin embargo, con un remojo completo antes de partir, agrupando todas las plantas juntas y preferiblemente en un lugar más sombrío.</p>
-        
-        <p>Tus plantas deberían poder sobrevivir durante 7 días. Para periodos de tiempo más prolongados, le sugerimos que hable con alguien más para que los riegue por usted...</p>
-       
-       
-        <h4>CÓMO ELEGIR LA PLANTA CORRECTA</h4>
+                <section>
+                    <h4>He tenido plantas anteriormente y se me han muerto, ¿Cómo me ayudará PlantMe para cuidarlas mejor?</h4>
+                    <p>Todas nuestras plantas tienen su descripción individual, las cuales describen los espacios donde podrás establecer tu planta; como también, los principales cuidados que deben tener en términos de temperaturas, luz y riego. </p>
+                </section>
 
-        <h5>¿Que plantas debo elegir dependiendo la cantidad de luz que reciben mis espacios?</h5>
-        <p>La mayoría de las veces podrás encontrar plantas que se adecuen a tus espacios. Hay plantas que necesitan lugares luminosos y algunas lugares sombríos, como también hay otras que se adaptan a los lugares intermedios, semi sombra.</p>
+                <section>
+                    <h4>¿Son nuestras plantas difíciles de mantener?</h4>
+                    <p>Todas las plantas requieren de cuidados, si bien hay algunas que requieren cuidados más complejos, existen otras que son de muy fácil cuidado, ¨Las resistentes¨. A pesar de que estas requieren de pocos cuidados, todavía no hemos encontrado un tipo de planta que no requiera de algún cuidado.</p>
+                    <p>De todas maneras, en nuestra página encontrarás información importante para hacer del cuidado de tus plantas lo más fácil posible. Además, con nuestros videos podrás aprender suficientes tips para convertirte en un experto en el cuidado de plantas.</p>
+                </section>
 
-        <p>Al momento de tu compra, sin importar si estas en alguna sección en especial o en plantas de interior en general, a la izquierda arriba podrás filtrar por plantas según la luminosidad de tus espacios.</p>
-       
-        <p>¿De que porte llegarán mis plantas y cuánto crecerán?</p>
-        <p>Todas nuestras plantas tienen su descripción individual, en ella sale el rango de altura de la planta y su precio respectivo. El porte que tu planta alcanzará dependerá del macetero en el cual se establezca.</p>
-       
-        <h4>TRASPLANTES</h4>
-        <h5>Si compro una planta y un macetero, ¿PlantMe hará el trasplante?</h5>
 
-        <p>Nuestras plantas ya vendrán en macetas, con tierra perfectamente abonada y sana. Estas macetas son de plástico con agujeros en su parte inferior, para que de esta manera se facilite el drenaje al regar. </p>
-        
-        <p>Tenemos también oferta de maceteros para que tu plantas se vean aún más elegantes y calcen de una mejor forma en tus espacios. Por el momento, nosotros transplantaremos las plantas por ti.</p>
+                <section>
+                    <h4>¿Mi planta seguirá viva a la vuelta de mis vacaciones?</h4>
+                    <p>Esto no se podrá saber con seguridad, y la respuesta a esta pregunta dependerá de distintos factores: de la cantidad de tiempo que no recibirá cuidado tu planta, del clima mientras está lejos, de la posición de las plantas con respecto al sol. Sin embargo, con un remojo completo antes de partir, agrupando todas las plantas juntas y preferiblemente en un lugar más sombrío.</p>
+                    <p>Tus plantas deberían poder sobrevivir durante 7 días. Para periodos de tiempo más prolongados, le sugerimos que hable con alguien más para que los riegue por usted...</p>
+                </section>
 
-        <h5>¿Como transplantar una planta  a otro macetero?</h5>
+                <section>
+                    <h4>CÓMO ELEGIR LA PLANTA CORRECTA</h4>
+                    
+                    <h5>¿Que plantas debo elegir dependiendo la cantidad de luz que reciben mis espacios?</h5>
+                    <p>La mayoría de las veces podrás encontrar plantas que se adecuen a tus espacios. Hay plantas que necesitan lugares luminosos y algunas lugares sombríos, como también hay otras que se adaptan a los lugares intermedios, semi sombra.</p>
+                    <p>Al momento de tu compra, sin importar si estas en alguna sección en especial o en plantas de interior en general, a la izquierda arriba podrás filtrar por plantas según la luminosidad de tus espacios.</p>
+                    <p>¿De que porte llegarán mis plantas y cuánto crecerán?</p>
+                    <p>Todas nuestras plantas tienen su descripción individual, en ella sale el rango de altura de la planta y su precio respectivo. El porte que tu planta alcanzará dependerá del macetero en el cual se establezca.</p>
+                </section>
 
-        <p>Trasplantar una planta es algo memorable, necesitarás un macetero que sea igual o mas grande que el que ya tiene tu planta. Siempre que trasplantes se recomienda poner tu planta en un macetero al menos un poco más grande que al anterior, de esta forma permitirás que tu planta pueda crecer en altura y follaje.</p>
-        
-        <p>Primero, debes mojar la tierra de tu planta 3 horas antes del trasplante, este paso es opcional pero te facilitará el trabajo, ya que de esta forma la planta saldrá de manera más fácil de su antiguo macetero. </p>
-        
-        <p>Segundo, para sacar la planta del antiguo macetero debes apretar el macetero por los costados para ir soltando la planta con la tierra, una vez que esté más suelta, dala vuelta procurando con una mano afirmar la planta y la parte posterior de la tierra, y con la otra jalar el macetero hacia afuera.  </p>
-        
-        <p>Tercero, incorpora sustrato en el macetero nuevo, procurando llenar 2,5 cm del fondo para que las raíces de tu planta no queden pegadas a la maceta. No uses cualquier tierra, ya que al no usar la tierra indicada tu planta puede sufrir alguna enfermedad. Nosotros tenemos a tu disposición tierra bien formulada y creada de manera que tu planta quede en las mejores condiciones y pueda vivir lo suficiente.</p>
-        
-        <p>Cuarto, suelta la tierra de la planta para que las raíces puedan tomar forma en el nuevo macetero. Luego, coloca la planta con las raíces sobre el sustrato ya colocado. Una vez que la tengas ahí, incorpora tierra hasta arriba procurando dejar 2,5 cm desde la tierra hasta la parte de arriba del macetero.</p>
-        
-        <p>Quinto, aprieta la tierra con las manos, teniendo en cuenta que si se hunde mucho habría que incorporar más tierra aún hasta llegar al punto mencionado anteriormente. Una vez terminado eso, riega abundantemente la planta para que las raíces y la tierra se adhieran entre ellas.</p>
+                <section>
+                    <h4>TRASPLANTES</h4>
+                    
+                    <h5>Si compro una planta y un macetero, ¿PlantMe hará el trasplante?</h5>
+                    <p>Nuestras plantas ya vendrán en macetas, con tierra perfectamente abonada y sana. Estas macetas son de plástico con agujeros en su parte inferior, para que de esta manera se facilite el drenaje al regar. </p>
+                    <p>Tenemos también oferta de maceteros para que tu plantas se vean aún más elegantes y calcen de una mejor forma en tus espacios. Por el momento, nosotros transplantaremos las plantas por ti.</p>
+                    
+                    <h5>¿Como transplantar una planta  a otro macetero?</h5>
+                    <p>Trasplantar una planta es algo memorable, necesitarás un macetero que sea igual o mas grande que el que ya tiene tu planta. Siempre que trasplantes se recomienda poner tu planta en un macetero al menos un poco más grande que al anterior, de esta forma permitirás que tu planta pueda crecer en altura y follaje.</p>
+                    <p>Primero, debes mojar la tierra de tu planta 3 horas antes del trasplante, este paso es opcional pero te facilitará el trabajo, ya que de esta forma la planta saldrá de manera más fácil de su antiguo macetero. </p>
+                    <p>Segundo, para sacar la planta del antiguo macetero debes apretar el macetero por los costados para ir soltando la planta con la tierra, una vez que esté más suelta, dala vuelta procurando con una mano afirmar la planta y la parte posterior de la tierra, y con la otra jalar el macetero hacia afuera.  </p>
+                    <p>Tercero, incorpora sustrato en el macetero nuevo, procurando llenar 2,5 cm del fondo para que las raíces de tu planta no queden pegadas a la maceta. No uses cualquier tierra, ya que al no usar la tierra indicada tu planta puede sufrir alguna enfermedad. Nosotros tenemos a tu disposición tierra bien formulada y creada de manera que tu planta quede en las mejores condiciones y pueda vivir lo suficiente.</p>
+                    <p>Cuarto, suelta la tierra de la planta para que las raíces puedan tomar forma en el nuevo macetero. Luego, coloca la planta con las raíces sobre el sustrato ya colocado. Una vez que la tengas ahí, incorpora tierra hasta arriba procurando dejar 2,5 cm desde la tierra hasta la parte de arriba del macetero.</p>    
+                    <p>Quinto, aprieta la tierra con las manos, teniendo en cuenta que si se hunde mucho habría que incorporar más tierra aún hasta llegar al punto mencionado anteriormente. Una vez terminado eso, riega abundantemente la planta para que las raíces y la tierra se adhieran entre ellas.</p>
+                </section>
+            </div>
+        </div>
     </div>
     <?php include '../../components/footer.php' ?>
 </body>
