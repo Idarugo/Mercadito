6df738d0da59fcfd21fd704260435cc6186203ca
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2022-10-16 21:40:34 -0300

    Tienda online fixed

--- pages/tiendaonline.php
+++ pages/tiendaonline.php
@@ -14,81 +14,79 @@
         <h1>Todos</h1>
         <p></p>
     </section>
-    <div class="container container-main">
-        
-    <article id="3685" class="categoria-listing">
-  <a class="categoria-title" href="#">Planta Interior</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-interior.jpg" alt="Planta Interior">    </a>
-  </div>
-</article>
-
-<article id="3686" class="categoria-listing">
-  <a class="categoria-title" href="#">Planta Exterior</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-exterior.jpg" alt="Planta Exterior">    </a>
-  </div>
-</article>
-
-<article id="3687" class="categoria-listing">
-  <a class="categoria-title" href="#">Planta Medicinales</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-medicinales.jpg" alt="Planta Medicinales">    </a>
-  </div>
-</article>
-
-<article id="3688" class="categoria-listing">
-  <a class="categoria-title" href="#">Maceteros</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-macetero.jpg" alt="Maceteros">    </a>
-  </div>
-</article>
-
-<article id="3689" class="categoria-listing">
-  <a class="categoria-title" href="#">Acessorio</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-accesorio.jpg" alt="Acessorio">    </a>
-  </div>
-</article>
-
-<article id="3687" class="categoria-listing">
-  <a class="categoria-title" href="#">Ofertas</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-oferta.jpg" alt="Ofertas">    </a>
-  </div>
-</article>
-
-<article id="3687" class="categoria-listing">
-  <a class="categoria-title" href="#">Nuevos Productos</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-nuevoproducto.jpg" alt="Nuevos Productos">    </a>
-  </div>
-</article>
-
-<article id="3687" class="categoria-listing">
-  <a class="categoria-title" href="#">Insumos</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-insumos.jpg" alt="Insumos">    </a>
-  </div>
-</article>
-
-<article id="3687" class="categoria-listing">
-  <a class="categoria-title" href="#">Flores</a>
-  <div class="categoria-image">
-    <a href="#">
-        <img  class="img_categories" src="../assets/images/categories/plant-flores.jpg" alt="Flores">    </a>
-  </div>
-</article>
-
-    </article>
+    <div class="container container-main container-plants"> 
+      <article id="3685" class="categoria-listing">
+        <a class="categoria-title" href="#">Planta Interior</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img class="img_categories" src="../assets/images/categories/plant-interior.jpg" alt="Planta Interior">    
+          </a>
+        </div>
+      </article>
+      <article id="3686" class="categoria-listing">
+        <a class="categoria-title" href="#">Planta Exterior</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img class="img_categories" src="../assets/images/categories/plant-exterior.jpg" alt="Planta Exterior">   
+          </a>
+        </div>
+      </article>
+      <article id="3687" class="categoria-listing">
+        <a class="categoria-title" href="#">Planta Medicinales</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img class="img_categories" src="../assets/images/categories/plant-macetero.jpg" alt="Planta Medicinales">    
+          </a>
+        </div>
+      </article>
+      <article id="3688" class="categoria-listing">
+        <a class="categoria-title" href="#">Maceteros</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img class="img_categories" src="../assets/images/categories/plant-macetero.jpg" alt="Maceteros">    
+          </a>
+        </div>
+      </article>
+      <article id="3689" class="categoria-listing">
+        <a class="categoria-title" href="#">Acessorio</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img class="img_categories" src="../assets/images/categories/plant-accesorio.jpg" alt="Acessorio">   
+          </a>
+        </div>
+      </article>
+      <article id="3687" class="categoria-listing">
+        <a class="categoria-title" href="#">Ofertas</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img  class="img_categories" src="../assets/images/categories/plant-oferta.jpg" alt="Ofertas">   
+          </a>
+        </div>
+      </article>
+      <article id="3687" class="categoria-listing">
+        <a class="categoria-title" href="#">Nuevos Productos</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img  class="img_categories" src="../assets/images/categories/plant-nuevoproducto.jpg" alt="Nuevos Productos">   
+          </a>
+        </div>
+      </article>
+      <article id="3687" class="categoria-listing">
+        <a class="categoria-title" href="#">Insumos</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img class="img_categories" src="../assets/images/categories/plant-insumos.jpg" alt="Insumos">    
+          </a>
+        </div>
+      </article>
+      <article id="3687" class="categoria-listing">
+        <a class="categoria-title" href="#">Flores</a>
+        <div class="categoria-image">
+          <a href="#">
+            <img class="img_categories" src="../assets/images/categories/plant-flores.jpg" alt="Flores">    
+          </a>
+        </div>
+      </article>
     </div>
     <?php include '../components/footer.php' ?>
 </body>
