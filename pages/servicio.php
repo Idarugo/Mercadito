0304c45cbfc71237a3367719a40c0b2f0296eebd
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2022-10-16 23:25:38 -0300

    integrar banner para products

--- pages/servicio.php
+++ pages/servicio.php
@@ -17,12 +17,14 @@
         <p>A domicilios y empresa</p>
     </section>
     <div class="container container-main">
+        
+        <section>
+            <p>En <strong> Mercadito</strong> tenemos la misión de  <strong>darle vida a los ambientes</strong> , trayendo lo mejor de la naturaleza a hogares, lugares de trabajo y espacios personales.</p>
+        </section>
 
-        <h4>En <strong> Mercadito</strong> tenemos la misión de  <strong>darle vida a los ambientes</strong> , trayendo lo mejor de la naturaleza a hogares, lugares de trabajo y espacios personales. </h4>
-        
-        <h4>Actualmente nos hemos desarrollado en el área de <strong>creación, asesoría y mantención de espacios verdes </strong>  en hoteles, oficinas, restaurantes, tiendas, centros comerciales y muchos otros lugares que necesitan de un espacio verde.</h4>
-        
-        
+        <section>
+            <p>Actualmente nos hemos desarrollado en el área de <strong>creación, asesoría y mantención de espacios verdes </strong>  en hoteles, oficinas, restaurantes, tiendas, centros comerciales y muchos otros lugares que necesitan de un espacio verde.</p>
+        </section> 
     </div>
     <?php include '../components/footer.php' ?>
 </body>
