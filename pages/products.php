7e57c7f36c867c2023c6b94eb9024dd724668df1
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2022-10-17 21:34:03 -0300

    products

--- pages/products.php
+++ pages/products.php
@@ -9,7 +9,6 @@
 <?php include '../components/header.php' ?>
     <div class="container">
         <h1></h1>
-        
     </div>
     <section class="container-fluid banner">
         <div class="banner__img"></div>
@@ -17,8 +16,156 @@
         <p></p>
     </section>
     <div class="container container-main">
-        
-    </div>
+<div class="collection_filter_item">50 Articulos</div>
+
+<div class="container container-main container-plants"> 
+    
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+            <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+      <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+       <article  class="categoria-plant">
+        <a class="categoria-title" href="../assets/images/fondoplanta-temporal.jpg""></a>
+        <div class="plant_secondary-image">
+          <a href="#">
+          <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="">
+          </a>
+        </div>
+      </article>
+
+  </div>
+</div>
     <?php include '../components/footer.php' ?>
 </body>
 </html>
