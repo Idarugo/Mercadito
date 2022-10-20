c6de84a3ccad37189d84844e9faffbe728309309
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2022-10-20 09:05:48 -0300

    arreglo index

--- components/header.php
+++ components/header.php
@@ -15,7 +15,7 @@
         <li onclick="redirectTo('pages/tiendaonline.php')" class="header__menu-item">Tienda Online</li>
         <li onclick="redirectTo('pages/nuestrojardin.php')" class="header__menu-item">Nuestro Jardin</li>
         <li onclick="redirectTo('pages/blog.php')" class="header__menu-item">Blog</li>
-        <div onclick="redirectTo('/index.php')"  class="header__logo"></div>
+        <div onclick="redirectTo('./index.php')"  class="header__logo"></div>
         <li onclick="redirectTo('pages/nosotros.php')" class="header__menu-item">Nosotros</li>
         <li onclick="redirectTo('pages/servicio.php')" class="header__menu-item">Servicios</li>
         <li onclick="redirectTo('pages/contactenos.php')" class="header__menu-item">Contactanos</li>
--- index.php
+++ index.php
@@ -17,17 +17,17 @@
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="5000">
-          <img src="../assets/images/carrusel1.mp4" class="d-block w-100" alt="">
+          <img src="./assets/images/carrusel1.mp4" class="d-block w-100" alt="">
           <div class="carousel-caption d-none d-md-block">
           </div>
         </div>
         <div class="carousel-item" data-bs-interval="5000">
-          <img src="../assets/images/carrusel2.png" class="d-block w-100" alt="...">
+          <img src="./assets/images/carrusel2.png" class="d-block w-100" alt="...">
           <div class="carousel-caption d-none d-md-block">
           </div>
         </div>
         <div class="carousel-item" data-bs-interval="9000">
-          <img src="../assets/images/carrusel3.mp4"  class="d-block w-100" alt="...">
+          <img src="./assets/images/carrusel3.mp4"  class="d-block w-100" alt="...">
           <div class="carousel-caption d-none d-md-block">
           </div>
         </div>
@@ -45,7 +45,7 @@
     <div class="row mb-2">
         <div class="col aling-self-center" >
         <div class="col-11">
-        <img class="img1" src="../assets/images/Te-esperamos.png " href="https://www.google.com/maps/place/34°08'28.7%22S+70°46'19.9%22W/@-34.1413002,-70.7743826,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x2c597ed84a9ca751!8m2!3d-34.1413002!4d-70.7721939?hl=es">
+        <img class="img1" src="./assets/images/Te-esperamos.png " href="https://www.google.com/maps/place/34°08'28.7%22S+70°46'19.9%22W/@-34.1413002,-70.7743826,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x2c597ed84a9ca751!8m2!3d-34.1413002!4d-70.7721939?hl=es">
              </div>
         </div>
     </div>
@@ -55,25 +55,22 @@
     <div class="container text-center">
       <div class="row">
         <div class="col">
-        <img src="../assets/images/instagram1.jpg" href="https://www.instagram.com/mercadito_jessicamoreno" class="d-block w-100" alt="...">
+        <img src="./assets/images/instagram1.jpg" href="https://www.instagram.com/mercadito_jessicamoreno" class="d-block w-100" alt="...">
         </div>
         <div class="col">
-        <img src="../assets/images/instagram2.jpg" class="d-block w-100" alt="...">
+        <img src="./assets/images/instagram2.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="col">
-        <img src="../assets/images/instagram3.jpg" class="d-block w-100" alt="...">
+        <img src="./assets/images/instagram3.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="col">
-        <img src="../assets/images/instagram4.jpg" class="d-block w-100" alt="...">
+        <img src="./assets/images/instagram4.jpg" class="d-block w-100" alt="...">
         </div>
       </div>
     </div>
 
     <div class="mb-2"></div>
-
-
-        
-
+    
     </div>
     <?php include './components/footer.php' ?>
 </body>
