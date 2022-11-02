d685417aded8ee22fcc1b1b44cad5e91c792a417
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2022-11-02 16:43:33 -0300

    camibo product

--- controllers/plant.controller.php
+++ controllers/plant.controller.php
@@ -1,79 +1,81 @@
 <?php
-    require '../models/Plants.php';
+require '../models/Plants.php';
 
-    class PlantController {
-    
-        private $connectDB;
+class PlantController
+{
 
-        function __construct( $connectDB)
-        {
-            $this->connectDB = $connectDB;
-        }
+    private $connectDB;
 
-
-        public function registerPlants($title,$price,$description,$category,$image,$cant,$typeDelivery){
-            $this->connectDB->connect();
-            $sql = "INSERT INTO `plants`(`title`, `price`, `description`, `category`, `image`, `cant`, `type_delivery`) VALUES ('$title','$price','$description','$category','$image','$cant','$typeDelivery')";
-            $this->connectDB->query($sql);
-            if($this->connectDB->getDB()->affected_rows){
-                $this->connectDB->disconnect();
-                header("location:  ../pages/agregarproductos.php?created");
-                return;
-            }
-            $this->connectDB->disconnect();
-            header("location:  ../pages/agregarproductos.php?RegisterError");
-            return;
-        }
-
- 
-
-        public function getPlant($id){
-            session_start();
-            $this->connectDB->connect();
-            $sql = "SELECT * FROM plants";
-            $resp = $this->connectDB->query($sql);
-            $list = array();
-            while($rs = mysqli_fetch_array($resp)){
-                $plants = new Plant($rs[0],$rs[1],$rs[2],$rs[3],$rs[4],$rs[5],$rs[6],$rs[7],$rs[8],$rs[9],$rs[10]);
-                $list[] = $plants;
-            }
-            $_SESSION['plants'] = $list;
-            $this->connectDB->disconnect();
-            header("location:  $id");
-            return;
-        }
-        public function getAllPlantsByIdCategory($id){
-            $lista = array();
-            $this->connectDB->connect();
-            $sql = "SELECT * FROM `plants` WHERE `category`= $id";
-            $st = $this->connectDB->query($sql);
-            while($rs = mysqli_fetch_array($st)){
-                $lista[] = new Plant($rs['id'],$rs['title'],$rs['price'],$rs['description'],$rs['category'],$rs['image'],$rs['cant'],$rs['type_delivery']);;
-            }
-            $this->connectDB->disconnect();
-            return $lista;
-        }
-        public function select(){
-            $lista = array();
-            $this->connectDB->connect();
-            $sql = " SELECT * FROM `plants`";
-            $st = $this->connectDB->query($sql);
-            while($rs = mysqli_fetch_array($st)){
-                $lista[] = new Plant($rs['id'],$rs['title'],$rs['price'],$rs['description'],$rs['category'],$rs['image'],$rs['cant'],$rs['type_delivery']);;
-            }
-            $this->connectDB->disconnect();
-            return $lista;
-        }
-
-        public function eliminarplant($id){
-            $this->connectDB->connect();
-            $sql = "DELETE FROM plants WHERE id = $id";
-            $st = $this->connectDB->query($sql);
-            $this->connectDB->disconnect();
-          }
-
-        
+    function __construct($connectDB)
+    {
+        $this->connectDB = $connectDB;
     }
 
 
-?> 
+    public function registerPlants($title, $price, $description, $category, $image, $cant, $typeDelivery)
+    {
+        $this->connectDB->connect();
+        $sql = "INSERT INTO `plants`(`title`, `price`, `description`, `category`, `image`, `cant`, `type_delivery`) VALUES ('$title','$price','$description','$category','$image','$cant','$typeDelivery')";
+        $this->connectDB->query($sql);
+        if ($this->connectDB->getDB()->affected_rows) {
+            $this->connectDB->disconnect();
+            header("location:  ../pages/agregarproductos.php?created");
+            return;
+        }
+        $this->connectDB->disconnect();
+        header("location:  ../pages/agregarproductos.php?RegisterError");
+        return;
+    }
+
+
+
+    public function getPlant($id)
+    {
+        session_start();
+        $this->connectDB->connect();
+        $sql = "SELECT * FROM plants";
+        $resp = $this->connectDB->query($sql);
+        $list = array();
+        while ($rs = mysqli_fetch_array($resp)) {
+            $plants = new Plant($rs[0], $rs[1], $rs[2], $rs[3], $rs[4], $rs[5], $rs[6], $rs[7], $rs[8], $rs[9], $rs[10]);
+            $list[] = $plants;
+        }
+        $_SESSION['plants'] = $list;
+        $this->connectDB->disconnect();
+        header("location:  $id");
+        return;
+    }
+    public function getAllPlantsByIdCategory($id)
+    {
+        $lista = array();
+        $this->connectDB->connect();
+        $sql = "SELECT * FROM `plants` WHERE `category`= $id";
+        $st = $this->connectDB->query($sql);
+        while ($rs = mysqli_fetch_array($st)) {
+            $lista[] = new Plant($rs['id'], $rs['title'], $rs['price'], $rs['description'], $rs['category'], $rs['image'], $rs['cant'], $rs['type_delivery']);;
+        }
+        $this->connectDB->disconnect();
+        return $lista;
+    }
+
+    public function select()
+    {
+        $lista = array();
+        $this->connectDB->connect();
+        $sql = " SELECT * FROM `plants`";
+        $st = $this->connectDB->query($sql);
+        while ($rs = mysqli_fetch_array($st)) {
+            $lista[] = new Plant($rs['id'], $rs['title'], $rs['price'], $rs['description'], $rs['category'], $rs['image'], $rs['cant'], $rs['type_delivery']);;
+        }
+        $this->connectDB->disconnect();
+        return $lista;
+    }
+
+    public function eliminarplant($id)
+    {
+        $this->connectDB->connect();
+        $sql = "DELETE FROM plants WHERE id = $id";
+        $st = $this->connectDB->query($sql);
+        $this->connectDB->disconnect();
+    }
+}
--- pages/products.php
+++ pages/products.php
@@ -1,61 +1,61 @@
 <?php
-  require '../core/bootstraper.php';
-  require '../controllers/plant.controller.php';
-  if(!isset($_GET['category'])){
-    header("location:  ./tiendaonline.php");
-  }
-  $plants = new PlantController($connectDB);
-  $plantsByIdCategory = $plants->getAllPlantsByIdCategory($_GET['category']);
+require '../core/bootstraper.php';
+require '../controllers/plant.controller.php';
+if (!isset($_GET['category'])) {
+  header("location:  ./tiendaonline.php");
+}
+$plants = new PlantController($connectDB);
+$plantsByIdCategory = $plants->getAllPlantsByIdCategory($_GET['category']);
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <?php include '../components/head.php' ?>
-    <link rel="stylesheet" href="../assets/styles/main.css">
-    <link rel="stylesheet" href="../assets/styles/pages/products.css">
+  <?php include '../components/head.php' ?>
+  <link rel="stylesheet" href="../assets/styles/main.css">
+  <link rel="stylesheet" href="../assets/styles/pages/products.css">
 </head>
+
 <body>
-<?php include '../components/header.php' ?>
+  <?php include '../components/header.php' ?>
 
-    <div class="container">
-        <h1></h1>
-    </div>
-    <section class="container-fluid banner">
-        <div class="banner__img"></div>
-        <h1></h1>
-        <p></p>
-    </section>
-    <div class="container container-main">
-<div class="collection_filter_item"><?= count($plantsByIdCategory);?> Articulos</div>
+  <div class="container">
+    <h1></h1>
+  </div>
+  <section class="container-fluid banner">
+    <div class="banner__img"></div>
+    <h1></h1>
+    <p></p>
+  </section>
+  <div class="container container-main">
+    <div class="collection_filter_item"><?= count($plantsByIdCategory); ?> Articulos</div>
 
-<div class="container container-main container-plants"> 
-    
-  <?php
-    if(count($plantsByIdCategory) == 0){
-      echo 'No hay Productos';
-    }
-    for($i = 0; $i < count($plantsByIdCategory); $i++){
-      echo '
+    <div class="container container-main container-plants">
+
+      <?php
+      if (count($plantsByIdCategory) == 0) {
+        echo 'No hay Productos';
+      }
+      for ($i = 0; $i < count($plantsByIdCategory); $i++) {
+        echo '
       <article  class="categoria-plant">
-        <a class="categoria-title" href="../pages/plantdetail.php">'.$plantsByIdCategory[$i]->geTitle().'</a>
+        <a class="categoria-title" href="../pages/plantdetail.php">' . $plantsByIdCategory[$i]->geTitle() . '</a>
         <div class="plant_secondary-image">
           <a href="#">
-            <img class="img_planta" src="../assets/images/fondoplanta-temporal.jpg" alt="'.$plantsByIdCategory[$i]->geTitle().'">
+            <img class="img_planta" src="data:imagen/jpg;base64,' . base64_encode($plantsByIdCategory[$i]->getImage()) . '" alt="' . $plantsByIdCategory[$i]->geTitle() . '">
           </a>
         </div>
       </article>
       ';
-    }
-  ?>
-      
+      }
+      ?>
 
+
+    </div>
+    <br />
   </div>
-  <br/>
-</div>
-    <?php include '../components/footer.php' ?>
+  <?php include '../components/footer.php' ?>
 </body>
+
 </html>
-
-
-
