71d698254c5439f1967cf972b1e54b19dcb35de5
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2022-11-04 09:21:32 -0300

    todo perfil 1.0

--- pages/perfil.php
+++ pages/perfil.php
@@ -30,7 +30,7 @@
                 </div>
                 <div class="col">
                     <h3>Detalles de la cuenta</h3>
-                    <center><img class="fotoperfil img-circle" src="data:imagen/jpg;base64,<?php echo base64_encode($user->getImagen()); ?>"></center>
+                    <center><img class="fotoperfil rounded-circle" src="data:imagen/jpg;base64,<?php echo base64_encode($user->getImagen()); ?>"></center>
                     <h5><?php echo $user->getNombre(); ?></h5>
                     <h5><?php echo $user->getCorreo(); ?></h5>
                     <h5>******</h5>
@@ -55,7 +55,7 @@
             </div>
             <div class="col">
                 <h3>Detalles de la cuenta</h3>
-                <center><img class="rounded-circle" src="data:imagen/jpg;base64,<?php echo base64_encode($user->getImagen()); ?>"></center>
+                <center><img class="fotoperfil rounded-circle" src="data:imagen/jpg;base64,<?php echo base64_encode($user->getImagen()); ?>"></center>
                 <h5><?php echo $user->getNombre(); ?></h5>
                 <h5><?php echo $user->getCorreo(); ?></h5>
                 <h5>******</h5>
